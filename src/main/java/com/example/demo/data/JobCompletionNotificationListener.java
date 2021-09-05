74f09be977f260971b7d0ecda73a86f6942029b7
Niroshan-Krishnamoorthy <niro.dev97@gmail.com>
2021-09-05 16:33:00 +0530

    batch processing job created

---
+++ src/main/java/com/example/demo/data/JobCompletionNotificationListener.java
@@ -0,0 +1,34 @@
+package com.example.demo.data;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.BatchStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.listener.JobExecutionListenerSupport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JobCompletionNotificationListener extends JobExecutionListenerSupport {
+
+  private static final Logger log = LoggerFactory.getLogger(JobCompletionNotificationListener.class);
+
+  private final JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  public JobCompletionNotificationListener(JdbcTemplate jdbcTemplate) {
+    this.jdbcTemplate = jdbcTemplate;
+  }
+
+  @Override
+  public void afterJob(JobExecution jobExecution) {
+    if(jobExecution.getStatus() == BatchStatus.COMPLETED) {
+      log.info("!!! JOB FINISHED! Time to verify the results");
+
+      jdbcTemplate.query("SELECT date, team1, team2, toss_winner, toss_decision FROM match",
+        (rs, row) -> "Date=> "+rs.getString(1)+" Team 1=> "+rs.getString(2)+" Team 2=> "+rs.getString(3)+" Toss Winner=> "+rs.getString(4)+" Decision=> " + rs.getString(5)
+      ).forEach(str->System.out.println(str));
+    }
+  }
+}
---
+++ src/main/java/com/example/demo/data/MatchDataProcessor.java
@@ -0,0 +1,46 @@
+package com.example.demo.data;
+import java.time.LocalDate;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import org.springframework.batch.item.ItemProcessor;
+
+import com.example.demo.model.Match;
+
+
+public class MatchDataProcessor implements ItemProcessor<MatchInput, Match>{
+	private static final Logger log = LoggerFactory.getLogger(MatchDataProcessor.class);
+
+	  @Override
+	  public Match process(final MatchInput matchInput) throws Exception {
+	     Match match = new Match();
+	     
+	     match.setId(Long.parseLong(matchInput.getId()));
+	     match.setCity(matchInput.getCity());
+	     match.setDate(LocalDate.parse(matchInput.getDate()));
+	     match.setVenue(matchInput.getVenue());
+	     
+	     //select first batting team as the first innings team and the other as the second innings team
+	     String firstInningsTeam, secondInningsTeam;
+	     if(matchInput.getToss_decision().equals("bat")) {
+	    	 firstInningsTeam = matchInput.getToss_winner();
+	    	 secondInningsTeam = matchInput.getTeam1().equals(firstInningsTeam) ? matchInput.getTeam2() : matchInput.getTeam1();
+	     }else {
+	    	 secondInningsTeam = matchInput.getToss_winner();
+	    	 firstInningsTeam = matchInput.getTeam1().equals(secondInningsTeam) ? matchInput.getTeam2() : matchInput.getTeam1();
+	     }
+	     
+	     match.setTeam1(firstInningsTeam);
+	     match.setTeam2(secondInningsTeam);
+	     match.setTossWinner(matchInput.getToss_winner());
+	     match.setTossDecision(matchInput.getToss_decision());
+	     match.setResult(matchInput.getResult());
+	     match.setResultMargin(matchInput.getResult_margin());
+	     match.setUmpire1(matchInput.getUmpire1());
+	     match.setUmpire2(matchInput.getUmpire2());
+	     
+	     return match;
+	  }
+
+}
